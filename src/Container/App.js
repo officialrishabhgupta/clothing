d95ac4feb2763e599b64908af7e25afbab1023ef
officialrishabhgupta <rishabhgupta7210012474@gmail.com>
2022-01-04 22:13:18 +0530

    master

--- src/Container/App.js
+++ src/Container/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/homepage/homepage';
+import ShopPage from '../pages/shop/shop';
+
+
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<HomePage/>}/>
+          <Route path='/shop' element={<ShopPage/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
