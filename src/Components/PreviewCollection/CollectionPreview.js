17b6b6e7a3901e5282b3d612db51e5a8a7bea432
officialrishabhgupta <rishabhgupta7210012474@gmail.com>
2022-01-06 10:49:41 +0530

    master

--- src/Components/PreviewCollection/CollectionPreview.js
+++ src/Components/PreviewCollection/CollectionPreview.js
@@ -1,14 +1,15 @@
 import React from "react";
+import CollectionItem from "../Collection-item/Collection-item";
 import './CollectionPreview.scss';
 
-const CollectionPreview=({title,items})=>(
+const CollectionPreview=({title, items})=>(
     <div className="collection-preview">
         <h1 className="title">{title.toUpperCase()}</h1>
         <div className="preview">
                 {items
-                  .filter((item, idx)=>idx < 4)
-                  .map(item =>(
-                    <div key={item.id}>{item.name}</div>
+                  .filter((item, idx) => idx < 4)
+                  .map(({id, ...otherItemProps}) =>(
+                    <CollectionItem key={id}{...otherItemProps}/>
                 ))}
         </div>
     </div>
