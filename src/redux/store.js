ac9a931f45a93d28a5d6e7d388181f0311f7ce04
officialrishabhgupta <rishabhgupta7210012474@gmail.com>
2022-01-22 17:59:38 +0530

    mastr

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,11 +4,11 @@ import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [];
+const middlewares = [logger];
 
-if (process.env.NODE_ENV === 'development') {
-    middlewares.push(logger);
-}
+// if (process.env.NODE_ENV === 'development') {
+//     middlewares.push(logger);
+// }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
